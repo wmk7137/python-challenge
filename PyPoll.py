3fe1a237f51c2257239b6d70c47bab696e20245f
wmk7137 <164268291+wmk7137@users.noreply.github.com>
2024-04-22 11:47:22 -0400

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -1,9 +1,13 @@
+# Import Poll Data
+
 import os
 import csv
 
 poll_data = os.path.join('Resources', 'election_data.csv')
 
 print('Election Results')
+
+# Create variables & Dictionary
 
 total_votes = 0
 candidate_votes = {
@@ -12,10 +16,14 @@ candidate_votes = {
     "Raymon Anthony Doane": 0
 }
 
+# Opem csv file and reader
+
 with open(poll_data, 'r') as file_handler:
     csv_reader = csv.reader(file_handler)
     next(csv_reader)
 
+# Perform for loop for data ananlysis    
+    
     for row in csv_reader:
         total_votes += 1
         candidate_name = row[2]
@@ -25,6 +33,8 @@ with open(poll_data, 'r') as file_handler:
             candidate_votes["Diana DeGette"] += 1
         elif candidate_name == "Raymon Anthony Doane":
             candidate_votes["Raymon Anthony Doane"] += 1
+
+# Print results
 
 print("Total Votes:", total_votes)
 print("Candidate Votes:", candidate_votes)
